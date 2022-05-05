8b369aa1cd3e721e579e6bd75225241aba543caa
Igor Amorim <igor340@gmail.com>
2022-05-04 22:29:06 -0300

    CYOA: Starting the 'choose your own adventure' exercise

---
+++ cyoa/cmd/cyoaweb/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"cyoa"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
+	filename := flag.String("file", "gopher.json", "the JSON file with the story")
+	flag.Parse()
+	fmt.Printf("Using the story %s\n", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the application on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
---
+++ cyoa/story.go
@@ -0,0 +1,72 @@
+package cyoa
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose your own adventure</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+        <ul>
+            {{range .Options}}
+                <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+            {{end}}
+        </ul>
+    </body>
+</html>
+`
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
